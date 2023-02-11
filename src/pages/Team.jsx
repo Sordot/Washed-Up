5a33c41a9baaa43e852f5d7a0b9cdf9a2ca9d005
Sordot <tgevirtz@gmail.com>
2023-02-10 22:09:03 -0600

    centers grid on page

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -8,19 +8,25 @@ import tamales from '../assets/Tamales.png'
 import caribbean from '../assets/Caribbean.png'
 import hito from '../assets/Hito.jpg'
 import ardonis from '../assets/Ardonis.jpg'
+import dom from '../assets/Dom.png'
+import garumn from '../assets/Garumn.png'
+import scrotehorn from '../assets/Scrotehorn.png'
 
 const Team = () => {
 
     const guildRoster = [
         {id: 1, imgPath: oneechan, playerName: "Oneechan", role: "Guild Master", playerClass: "Paladin"},
         {id: 2, imgPath: brainoff, playerName: "Brainoff", role: "Raid Leader", playerClass: "Death Knight"},
-        {id: 3, imgPath: gargoyledeez, playerName: "Gargoyledeez", role: "Loot Master", playerClass: "Death Knight"},
-        {id: 4, imgPath: skonaz, playerName: "Skonaz", role: "Officer", playerClass: "Rogue"},
-        {id: 5, imgPath: ubet, playerName: "Ubet", role: "Core Raider", playerClass: "Warlock"},
-        {id: 6, imgPath: tamales, playerName: "Tamales", role: "Core Raider", playerClass: "Hunter"},
-        {id: 7, imgPath: caribbean, playerName: "Caribbean", role: "Core Raider", playerClass: "Priest"},
-        {id: 8, imgPath: hito, playerName: "Hito", role: "Core Raider", playerClass: "Shaman"},
-        {id: 9, imgPath: ardonis, playerName: "Ardonis", role: "Core Raider", playerClass: "Hunter"},
+        {id: 3, imgPath: dom, playerName: "Dom", role: "Raid Leader", playerClass: "Druid"},
+        {id: 4, imgPath: scrotehorn, playerName: "Scrotehorn", role: "Raid Leader", playerClass: "Druid"},
+        {id: 5, imgPath: gargoyledeez, playerName: "Gargoyledeez", role: "Loot Master", playerClass: "Death Knight"},
+        {id: 6, imgPath: skonaz, playerName: "Skonaz", role: "Officer", playerClass: "Rogue"},
+        {id: 7, imgPath: garumn, playerName: "Garumn", role: "Officer", playerClass: "Shaman"},
+        {id: 8, imgPath: ubet, playerName: "Ubet", role: "Core Raider", playerClass: "Warlock"},
+        {id: 9, imgPath: tamales, playerName: "Tamales", role: "Core Raider", playerClass: "Hunter"},
+        {id: 10, imgPath: caribbean, playerName: "Caribbean", role: "Core Raider", playerClass: "Priest"},
+        {id: 11, imgPath: hito, playerName: "Hito", role: "Core Raider", playerClass: "Shaman"},
+        {id: 12, imgPath: ardonis, playerName: "Ardonis", role: "Core Raider", playerClass: "Hunter"},
     ]
 
     return <>
