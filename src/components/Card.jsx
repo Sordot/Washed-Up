19ebec2a4d23c1a58583f39e97c8cc73939e0b00
Sordot <tgevirtz@gmail.com>
2023-02-25 17:32:01 -0600

    removed button added warcraftlogs favicon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,13 +4,19 @@ const Card = ({imgPath, playerName, playerRole, playerClass}) => {
 
     return <>
         <div className="card">
-            <img className="player-img" src={imgPath} alt="" />
-            <h3 className="player-name">{playerName}</h3>
+            <img className="player-img" src={imgPath} alt="Player" />
+            <div className='player-header'>
+                <h3 className="player-name">{playerName}</h3>
+                <a href={`https://classic.warcraftlogs.com/character/US/Ashkandi/${playerName}`}>
+                    <img className='warcraftlogs-logo'
+                        src="https://assets.rpglogs.com/img/warcraft/favicon.png?v=2" 
+                        alt="Warcraft Logs Logo" />
+                </a>
+            </div>
             <div className="player-summary">
                 <p className="guild-role">{playerRole}</p>
                 <p className="player-class">{playerClass}</p>
             </div>
-            <button className="btn"><a href={`https://classic.warcraftlogs.com/character/US/Ashkandi/${playerName}`}>Warcraftlogs!</a></button>
         </div>
     </>
 
