60de5189d6e49c380168797d882fe86953918811
Sordot <tgevirtz@gmail.com>
2023-02-25 20:27:47 -0600

    basic grid and content for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,33 @@
+import yogg from '../assets/yogg.png'
+import sarth from '../assets/Sarth10.jpg'
+import kt from '../assets/kt.jpg'
+import simian from '../assets/simian.png'
+import '../Home.css'
+
 const Home = () => {
     return <>
-        <h1>HOME PAGE</h1>
+        <div className="home-container">
+            <img className="home-image"src={yogg} alt="Yogg-Saron" />
+            <div className="home-text">
+                <h2>Our Misson: Defeat the Lich King</h2>
+                <p>Here is some more sample text</p>
+            </div>
+            <img className="home-image" src={sarth} alt="Sartharion" />
+            <div className="home-text">
+                <h2>Our Misson: Defeat the Lich King</h2>
+                <p>Here is some more sample text</p>
+            </div>
+            <img className="home-image" src={kt} alt="Sartharion" />
+            <div className="home-text">
+                <h2>Our Misson: Defeat the Lich King</h2>
+                <p>Here is some more sample text</p>
+            </div>
+            <img className="home-image" src={simian} alt="Sartharion" />
+            <div className="home-text">
+                <h2>Our Misson: Defeat the Lich King</h2>
+                <p>Here is some more sample text</p>
+            </div>
+        </div>
     </>
 }
 
