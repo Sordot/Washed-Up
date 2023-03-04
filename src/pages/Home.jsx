6be3d4256f430db16bf6ff0004639676eb5d28cf
Sordot <tgevirtz@gmail.com>
2023-03-03 20:55:09 -0600

    color update and content added to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,26 +6,33 @@ import '../Home.css'
 
 const Home = () => {
     return <>
+        <img className="banner" src={yogg} alt="yogg" />
+        <div className="lead-text">
+            <h1>Our Misson: <span id="lich-king">Defeat the Lich King</span></h1>
+        </div>
         <div className="home-container">
-            <img className="home-image"src={yogg} alt="Yogg-Saron" />
+            <img className="home-image" src={kt} alt="KT" />
             <div className="home-text">
-                <h2>Our Misson: Defeat the Lich King</h2>
-                <p>Here is some more sample text</p>
+                <h2 className='subheading'>Join our Guild</h2>
+                <ul className="benefits-list">
+                    <li className="subheading-copy">Washed Up is a semi-casual raiding guild focused on progression and ensuring all of our raiders get their fair share of the loot!</li>
+                    <li className="subheading-copy">We provide free consumables, enchantments, gems to all raiders who join us as core members of the guild!</li>
+                    <li className="subheading-copy">We offer recruitment bonuses to members who find new friends to join our ranks!</li>
+                </ul>
+                <div className="cta-wrapper">
+                    <button className="btn cta">Apply</button>
+                </div>
             </div>
             <img className="home-image" src={sarth} alt="Sartharion" />
             <div className="home-text">
-                <h2>Our Misson: Defeat the Lich King</h2>
-                <p>Here is some more sample text</p>
+                <h2 className='subheading'>Raid Times</h2>
+                <p className='subheading-copy schedule'>8 to 11pm Eastern</p>
+                <p className="subheading-copy schedule">Tuesday | Wednesday | Thursday</p>
             </div>
-            <img className="home-image" src={kt} alt="Sartharion" />
+            <img className="home-image" src={simian} alt="Super Simian Sphere" />
             <div className="home-text">
-                <h2>Our Misson: Defeat the Lich King</h2>
-                <p>Here is some more sample text</p>
-            </div>
-            <img className="home-image" src={simian} alt="Sartharion" />
-            <div className="home-text">
-                <h2>Our Misson: Defeat the Lich King</h2>
-                <p>Here is some more sample text</p>
+                <h2 className='subheading'>Our Misson: Defeat the Lich King</h2>
+                <p className='subheading-copy'></p>
             </div>
         </div>
     </>
